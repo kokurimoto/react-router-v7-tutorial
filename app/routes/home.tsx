f491789d8b1b73b9e6cf0181401ed4fca850064d
kokurimoto <koutarou.kurimoto@toda.co.jp>
2025-03-06 22:49:45 +0900

    refactor: biome check

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,14 @@
-import { Welcome } from "../welcome/welcome";
-import type { Route } from "./+types/home";
+import { Welcome } from '../welcome/welcome';
+import type { Route } from './+types/home';
 
 export const loader = () => {
   console.log('loader called!');
-}
+};
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
   ];
 }
 
